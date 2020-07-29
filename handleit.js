db5374092c361ea95974547463dc856856f4beaf
Joseph Bacal <bacalj@gmail.com>
2020-07-29 08:20:41 -0400

    identifying the items that we may want

--- handleit.js
+++ handleit.js
@@ -1,6 +1,18 @@
-let setBackTo = document.body.style.color;
+var setBackTo = document.body.style.color;
 
 document.body.style.backgroundColor = "orange";
+
+var lis = document.querySelectorAll('main ol li');
+
+lis.forEach(element => {
+    let isItem = element.classList.length > 4;
+    
+    if (isItem){
+        console.log(element);
+        element.style.backgroundColor = "yellow";
+    }
+    
+});
 
 setTimeout(() => {
     document.body.style.backgroundColor = setBackTo;
